a95711dda16bf47a47c0bc0e1b66ef414a6b25e2
MainPC <129085079+808Jade@users.noreply.github.com>
2024-11-18 03:27:34 +0900

    Update stdafx.h

--- C.G_termproject/C.G_project/stdafx.h
+++ C.G_termproject/C.G_project/stdafx.h
@@ -1,24 +1,30 @@
-#define _CRT_SECURE_NO_WARNINGS //--- 프로그램 맨 앞에 선언할 것
-#define Engin "C:/Users/kimkh/Desktop/C.G_test/C.G_project/C.G_project/sound/engins.mp3"
-#define SOUND_FILE_NAME_MIS "C:/Users/kimkh/Desktop/C.G_test/C.G_project/C.G_project/sound/missile.wav"
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdlib.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
-#include <gl/glew.h>
-#include <gl/freeglut.h>
-#include <gl/freeglut_ext.h>
-#include <gl/glm/glm.hpp>
-#include <gl/glm/ext.hpp>
-#include <gl/glm/gtc/matrix_transform.hpp>
 #include <random>
 #include <string>
-#include <gl/glm/fwd.hpp>
 #include <chrono>
-#pragma comment(lib,"winmm.lib")
+
 #include <mmsystem.h>
 #include <Digitalv.h>
 
+#include <gl/glew.h>
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+
+#include <gl/glm/glm.hpp>
+#include <gl/glm/ext.hpp>
+#include <gl/glm/gtc/matrix_transform.hpp>
+#include <gl/glm/fwd.hpp>
+
+#pragma comment(lib,"winmm.lib")
+
 #define width 1200
 #define height 800
+
+#define Engin "C:/Users/kimkh/Desktop/C.G_test/C.G_project/C.G_project/sound/engins.mp3"
+#define SOUND_FILE_NAME_MIS "C:/Users/kimkh/Desktop/C.G_test/C.G_project/C.G_project/sound/missile.wav"
