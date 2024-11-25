2c3225cbd2d92c28f7ca60b8def296efc78b2a16
MainPC <129085079+808Jade@users.noreply.github.com>
2024-11-26 00:49:27 +0900

    코드 돌아가게 수정

--- C.G_termproject/C.G_project/NetWorkFunction.cpp
+++ C.G_termproject/C.G_project/NetWorkFunction.cpp
@@ -63,25 +63,25 @@ void CleanupNetwork() {
 }
 
 // 로그인 요청 함수
-//bool SendLoginRequest(const char* playerid) {
-//    if (sock == INVALID_SOCKET) {
-//        cout << "서버에 연결되어 있지 않습니다." << endl;
-//        return false;
-//    }
-//
-//    PacketID loginPacket;
-//    loginPacket.size = sizeof(PacketID);
-//    loginPacket.type = PACKET_ID;
-//    strncpy_s(loginPacket.id, playerid, MAX_ID_SIZE - 1);
-//    loginPacket.id[MAX_ID_SIZE - 1] = '\0';
-//
-//    int retval = send(sock, (char*)&loginPacket, sizeof(loginPacket), 0);
-//    if (retval == SOCKET_ERROR) {
-//        cout << "로그인 패킷 전송 실패: " << WSAGetLastError() << endl;
-//        return false;
-//    }
-//    return true;
-//}
+bool SendLoginRequest(const char* playerid) {
+    if (sock == INVALID_SOCKET) {
+        cout << "서버에 연결되어 있지 않습니다." << endl;
+        return false;
+    }
+
+    PacketID loginPacket;
+    loginPacket.size = sizeof(PacketID);
+    loginPacket.type = PACKET_ID;
+    strncpy_s(loginPacket.id, playerid, MAX_ID_SIZE - 1);
+    loginPacket.id[MAX_ID_SIZE - 1] = '\0';
+
+    int retval = send(sock, (char*)&loginPacket, sizeof(loginPacket), 0);
+    if (retval == SOCKET_ERROR) {
+        cout << "로그인 패킷 전송 실패: " << WSAGetLastError() << endl;
+        return false;
+    }
+    return true;
+}
 
 void err_quit(const char* msg) {
     LPVOID lpMsgBuf;
--- C.G_termproject/C.G_project/ServerToClient.h
+++ C.G_termproject/C.G_project/ServerToClient.h
@@ -1,10 +1,12 @@
 #pragma once
 #define WIN32_LEAN_AND_MEAN  // Windows.h와 Winsock.h 충돌 방지
+#include "PacketDefine.h"
+
 #include <WinSock2.h>
 #include <Windows.h>
 #include <ws2tcpip.h>
+
 #pragma comment(lib, "ws2_32.lib")  // Winsock 라이브러리 링크
-#include "PacketDefine.h"
 
 
 
@@ -20,32 +22,14 @@ extern int myID;  // 클라이언트의 ID
 // extern int nowID;  // 로그인 부분때문에 추가함
 
 //#define SERVERIP "192.168.45.121"  //  이건 노트북ip 주소 ->192.168.40.29 경민
-#define SERVERIP "172.30.1.72"  //  아연
-//#define SERVERIP "?"  //  민상
+//#define SERVERIP "172.30.1.72"  //  아연
+#define SERVERIP "192.168.219.102"  //  민상
 
 
 bool InitializeNetwork();
 void CleanupNetwork();
 // 로그인 요청 함수
-//bool SendLoginRequest(const char* playerid) {
-//    if (sock == INVALID_SOCKET) {
-//        cout << "서버에 연결되어 있지 않습니다." << endl;
-//        return false;
-//    }
-//
-//    PacketID loginPacket;
-//    loginPacket.size = sizeof(PacketID);
-//    loginPacket.type = PACKET_ID;
-//    strncpy_s(loginPacket.id, playerid, MAX_ID_SIZE - 1);
-//    loginPacket.id[MAX_ID_SIZE - 1] = '\0';
-//
-//    int retval = send(sock, (char*)&loginPacket, sizeof(loginPacket), 0);
-//    if (retval == SOCKET_ERROR) {
-//        cout << "로그인 패킷 전송 실패: " << WSAGetLastError() << endl;
-//        return false;
-//    }
-//    return true;
-//}
+bool SendLoginRequest(const char* playerid);
 void SendReadyClientToServer();
 void SendNotReadyClientToServer();
 void ReadyClient();
